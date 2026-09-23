40e175922349af3c8592f8c6adbeeb2d9542f42e
jc01rho <rkjnice@gmail.com>
2026-09-23 21:01:14 +0900

    chore(web): regenerate release bundle for the null agent fix

--- web/herdr-bootstrap.js
+++ web/herdr-bootstrap.js
@@ -1,5 +1,5 @@
 (() => {
-  const target = new URL(window.__HERDR_ENTRY__ || "/builds/0.20.11-364-9ea2c4661fc07814/index.html", location.origin);
+  const target = new URL(window.__HERDR_ENTRY__ || "/builds/0.20.11-365-73f215a556265ccc/index.html", location.origin);
   target.search = location.search;
   target.hash = location.hash;
   location.replace(target.href);
