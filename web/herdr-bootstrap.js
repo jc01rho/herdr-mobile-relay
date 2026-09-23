0ee55bfd0f320bf13c618142a2377b30243b72f2
jc01rho <rkjnice@gmail.com>
2026-09-23 21:32:29 +0900

    chore(web): regenerate release bundle for the vanished agent guards

--- web/herdr-bootstrap.js
+++ web/herdr-bootstrap.js
@@ -1,5 +1,5 @@
 (() => {
-  const target = new URL(window.__HERDR_ENTRY__ || "/builds/0.20.11-365-73f215a556265ccc/index.html", location.origin);
+  const target = new URL(window.__HERDR_ENTRY__ || "/builds/0.20.11-365-1107de0d944f947f/index.html", location.origin);
   target.search = location.search;
   target.hash = location.hash;
   location.replace(target.href);
